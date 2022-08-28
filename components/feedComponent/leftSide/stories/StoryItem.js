0d1665d5fd40ead93bdbeea9fe8ec7eb8ae06860
a1a2a3a4a5 <linub7@gmail.com>
2022-08-28 12:07:07 +0430

    stories section ui

--- components/feedComponent/leftSide/stories/StoryItem.js
+++ components/feedComponent/leftSide/stories/StoryItem.js
@@ -1,10 +1,15 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from 'next/image';
 
 const StoryItem = ({ user }) => {
   return (
     <div>
-      <Image src={user?.img} height={50} width={50} alt={user?.username} />
-      <p>{user?.username}</p>
+      <img
+        src={user?.img}
+        alt={user?.username}
+        className="h-14 rounded-full p-[1.5px] border-red-500 border-2 cursor-pointer hover:scale-110 transition-all duration-200 ease-out"
+      />
+      <p className="text-xs w-14 truncate text-gray-800">{user?.username}</p>
     </div>
   );
 };
