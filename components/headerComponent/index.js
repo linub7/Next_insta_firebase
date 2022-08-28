1039c6ca866510e0482fa47d044166cb2b26fb02
a1a2a3a4a5 <linub7@gmail.com>
2022-08-28 10:53:18 +0430

    make sticky navbar

--- components/headerComponent/index.js
+++ components/headerComponent/index.js
@@ -4,13 +4,15 @@ import RightSide from './RightSide';
 
 const HeaderComponent = () => {
   return (
-    <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
-      {/* Left */}
-      <LeftSide />
-      {/* Middle */}
-      <Middle />
-      {/* Right */}
-      <RightSide />
+    <div className="shadow-sm border-b sticky top-0 bg-white z-30">
+      <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
+        {/* Left */}
+        <LeftSide />
+        {/* Middle */}
+        <Middle />
+        {/* Right */}
+        <RightSide />
+      </div>
     </div>
   );
 };
