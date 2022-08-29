458bc962e1561486a720b7a9aa1a5182bed1c516
a1a2a3a4a5 <linub7@gmail.com>
2022-08-29 10:16:15 +0430

    make responsive index.js page

--- components/feedComponent/index.js
+++ components/feedComponent/index.js
@@ -2,9 +2,12 @@ import FeedLeftSide from './leftSide';
 
 const FeedComponent = () => {
   return (
-    <main>
-      {/* Left Side */}
-      <FeedLeftSide />
+    <main className="grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto">
+      <div className="md:col-span-2">
+        {/* Left Side */}
+        <FeedLeftSide />
+      </div>
+      <div className="hidden md:inline-grid md:col-span-1"></div>
       {/* Right Side */}
     </main>
   );
